9129d4ea4cd72e5c0d7fa06563f5bdc27ea7a1f2
Dhefinurkholik <dhefinurkholik9@gmail.com>
2022-11-05 11:52:25 -0700

    upload file

--- latihan 3.py
+++ latihan 3.py
@@ -1,4 +1,4 @@
-for i in range(0, 10):
-    for j in range(0,10):
-        print(i+j, end='')
-    print()
+for i in range(0 , 10):
+    for j in range(0 , 10):
+        print(i+j , end=' ')
+    print( )
